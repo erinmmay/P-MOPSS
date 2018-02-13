e09b17a708f458da41ada46b73ba469bcc12ec27
Erin Mackenzie May <ermay@umich.edu>
2018-02-13 11:55:39 -0500

    Fixed y-shift detection. Updates to calling of function

--- AlignSpec.py
+++ AlignSpec.py
@@ -17,7 +17,7 @@ from setup import *
 def func_gaus(x,sigma):
     return 1.0-np.exp(-(1./2.)*(x/(sigma))**2.)
 
-def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex,binn,corr,skip):
+def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex,binn,corr,ver,time_trim,skip):
     masks=np.load(SAVEPATH+'FinalMasks.npz')['masks']
     if corr==True:
         input_data=np.load(SAVEPATH+'FlattenedSpectra_Corr.npz')['flat_spec']
@@ -27,16 +27,18 @@ def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex
     n_exp=input_data.shape[1]
     n_pix=input_data.shape[2]
     
-    cnv_data=np.empty([n_obj,n_exp,n_pix])
-    ovs_data=np.empty([n_obj,n_exp,osr*n_pix])
-    int_data=np.empty([n_obj,n_exp,n_pix])
+    cnv_data=np.empty([n_obj,n_exp,n_pix])*np.nan
+    ovs_data=np.empty([n_obj,n_exp,osr*n_pix])*np.nan
+    int_data=np.empty([n_obj,n_exp,n_pix])*np.nan
     
     pix_ar=np.linspace(n_pix-1,0,n_pix)
     pix_ar_os=np.linspace(n_pix-1,0,osr*n_pix)
     
-    shift_pixels=np.empty([n_obj,n_exp,n_pix])
+    shift_pixels=np.empty([n_obj,n_exp,n_pix])*np.nan
     
-    wav_ar=np.empty([n_obj,n_exp,n_pix])
+    wav_ar=np.empty([n_obj,n_exp,n_pix])*np.nan
+    
+    y_shift=np.empty([n_obj,n_exp])*np.nan
     
     
     #medfilt_data=medfilt(input_data,kernel_size=[1,1,window])
@@ -62,6 +64,7 @@ def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex
         #    continue
         counter=0
         
+        plt.figure(101,figsize=(12,5))
         for t in range(0,n_exp):
             if t%10==0:
                 plt.plot(np.linspace(0,2*ypixels+ygap,2*ypixels+ygap),input_data[o,t,:])
@@ -73,7 +76,7 @@ def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex
         plt.clf()
         
         print ' --Filtering...'
-        for t in range(0+2,n_exp-2):
+        for t in range(0+2,n_exp-2-time_trim):
             timemedian=medfilt(input_data[o,t,:],kernel_size=ks)
             data_medsb=input_data[o,t,:]-timemedian
             data_acmed=np.nanmedian(data_medsb)
@@ -87,7 +90,9 @@ def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex
                     val=timemedian[p]
                     input_data[o,t,p]=val
 
-        print '       -->>',counter
+        print '       -->>',counter/n_exp
+        
+        plt.figure(102,figsize=(12,5))
         for t in range(0,n_exp):
             if t%10==0:
                 plt.plot(np.linspace(0,2*ypixels+ygap-1,2*ypixels+ygap),input_data[o,t,:])
@@ -104,21 +109,22 @@ def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex
             width=2.*fwhm
             width_line=np.linspace(-width/2,width*2.,width)
             gaus_cnv=func_gaus(width_line,sigma)
-            for t in range(0,n_exp):
+            for t in range(0,n_exp-time_trim):
                 cnv_data[o,t,:]=convolve(input_data[o,t,:],gaus_cnv,'same')
             
         else:
-            fwhm_arr=np.load(SAVEPATH+'SpectraFitParams_'+str(int(o))+'.npz')['fwhm_av']
-            fwhm=fwhm_arr[t]
+            fwhm_arr=np.load(SAVEPATH+'FlattenedSpectra.npz')['fwhm_av']
+            fwhm=fwhm_arr[o,t]
             
-            for t in range(0,n_exp):
-                fwhm=fwhm_arr[t]
+            for t in range(0,n_exp-time_trim):
+                fwhm=fwhm_arr[o,t]
                 sigma=fwhm/2.355
                 width=2.*fwhm
                 width_line=np.linspace(-width/2,width*2.,width)
                 gaus_cnv=func_gaus(width_line,sigma)
                 cnv_data[o,t,:]=convolve(input_data[o,t,:],gaus_cnv,'same')
         
+        plt.figure(103,figsize=(12,5))
         for t in range(0,n_exp):
             if t%10==0:
                 plt.plot(np.linspace(0,2*ypixels+ygap-1,2*ypixels+ygap),cnv_data[o,t,:])
@@ -130,7 +136,7 @@ def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex
         plt.clf()
         
         print ' --Oversampling...'
-        for t in range(0,n_exp):
+        for t in range(0,n_exp-time_trim):
             interp_d=interp1d(pix_ar,cnv_data[o,t,:],bounds_error=False,fill_value=np.nan)
             ovs_data[o,t,:]=interp_d(pix_ar_os)
        
@@ -155,36 +161,36 @@ def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex
         wave_first=wav_func(pix_ar_os)
             
         #identifying line locations in first exposure
-        o2_7594_ind_upp=np.where(np.abs(wave_first-7589)==np.nanmin(np.abs(wave_first-7589)))[0][0]
-        o2_7594_ind_low=np.where(np.abs(wave_first-7599)==np.nanmin(np.abs(wave_first-7599)))[0][0]
+        o2_7594_ind_upp=np.where(np.abs(wave_first-(7594-30))==np.nanmin(np.abs(wave_first-(7594-30))))[0][0]
+        o2_7594_ind_low=np.where(np.abs(wave_first-(7594+30))==np.nanmin(np.abs(wave_first-(7594+30))))[0][0]
         print o2_7594_ind_low,o2_7594_ind_upp
         o2_7594_ar_0=ovs_data[o,0,o2_7594_ind_low:o2_7594_ind_upp]
         #o2_7594_ac_0=np.where(ovs_data[o,0,:]==np.nanmin(o2_7594_ar_0))[0][0]
         o2_7594_ac_0=np.argmin(o2_7594_ar_0)
         
         
-        o2_6867_ind_upp=np.where(np.abs(wave_first-6862)==np.nanmin(np.abs(wave_first-6862)))[0][0]
-        o2_6867_ind_low=np.where(np.abs(wave_first-6872)==np.nanmin(np.abs(wave_first-6872)))[0][0]
+        o2_6867_ind_upp=np.where(np.abs(wave_first-(6867-30))==np.nanmin(np.abs(wave_first-(6867-30))))[0][0]
+        o2_6867_ind_low=np.where(np.abs(wave_first-(6867+30))==np.nanmin(np.abs(wave_first-(6867+30))))[0][0]
         print o2_6867_ind_low,o2_6867_ind_upp
         o2_6867_ar_0=ovs_data[o,0,o2_6867_ind_low:o2_6867_ind_upp]
         #o2_6867_ac_0=np.where(ovs_data[o,0,:]==np.nanmin(o2_6867_ar_0))[0][0]
         o2_6867_ac_0=np.argmin(o2_6867_ar_0)
         
-        ha_6563_ind_upp=np.where(np.abs(wave_first-6558)==np.nanmin(np.abs(wave_first-6558)))[0][0]
-        ha_6563_ind_low=np.where(np.abs(wave_first-6568)==np.nanmin(np.abs(wave_first-6568)))[0][0]
+        ha_6563_ind_upp=np.where(np.abs(wave_first-(6563-30))==np.nanmin(np.abs(wave_first-(6563-30))))[0][0]
+        ha_6563_ind_low=np.where(np.abs(wave_first-(6563+30))==np.nanmin(np.abs(wave_first-(6563+30))))[0][0]
         print ha_6563_ind_low,ha_6563_ind_upp
         ha_6563_ar_0=ovs_data[o,0,ha_6563_ind_low:ha_6563_ind_upp]
         #ha_6563_ac_0=np.where(ovs_data[o,0,:]==np.nanmin(ha_6563_ar_0))[0][0]
         ha_6563_ac_0=np.argmin(ha_6563_ar_0)
         
-        na_5896_ind_upp=np.where(np.abs(wave_first-5891)==np.nanmin(np.abs(wave_first-5891)))[0][0]
-        na_5896_ind_low=np.where(np.abs(wave_first-5901)==np.nanmin(np.abs(wave_first-5901)))[0][0]
+        na_5896_ind_upp=np.where(np.abs(wave_first-(5896-30))==np.nanmin(np.abs(wave_first-(5896-30))))[0][0]
+        na_5896_ind_low=np.where(np.abs(wave_first-(5896+30))==np.nanmin(np.abs(wave_first-(5896+30))))[0][0]
         print na_5896_ind_low,na_5896_ind_upp
         na_5896_ar_0=ovs_data[o,0,na_5896_ind_low:na_5896_ind_upp]
         #na_5896_ac_0=np.where(ovs_data[o,0,:]==np.nanmin(na_5896_ar_0))[0][0]
         na_5896_ac_0=np.argmin(na_5896_ar_0)
         
-        for t in range(0,n_exp):
+        for t in range(0,n_exp-time_trim):
             #identify line locations in each exposure
             o2_7594_ar=ovs_data[o,t,o2_7594_ind_low:o2_7594_ind_upp]
             #o2_7594_ac=np.where(ovs_data[o,t,:]==np.nanmin(o2_7594_ar))[0][0]
@@ -210,6 +216,7 @@ def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex
             shift_na_5896=na_5896_ac_0-na_5896_ac
             
             shift=np.nanmedian([shift_o2_7594,shift_o2_6867,shift_ha_6563,shift_na_5896])
+            y_shift[o,t]=shift/osr
             
             if t%10==0:
                 print ' '
@@ -221,6 +228,60 @@ def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex
                 print 'na_5896 shift: ', shift_na_5896
             
                 print 'mean shift: ', shift, shift/osr
+                
+               
+                if ver==True:
+                    plt_ex=10
+            
+                    plt_o2_7594=ovs_data[o,:,o2_7594_ind_low-plt_ex:o2_7594_ind_upp+plt_ex]
+                    wav_7594=wave_first[o2_7594_ind_low:o2_7594_ind_upp]
+                
+                    plt_o2_6867=ovs_data[o,:,o2_6867_ind_low-plt_ex:o2_6867_ind_upp+plt_ex]
+                    wav_6867=wave_first[o2_6867_ind_low:o2_6867_ind_upp]
+                
+                    plt_ha_6563=ovs_data[o,:,ha_6563_ind_low-plt_ex:ha_6563_ind_upp+plt_ex]
+                    wav_6563=wave_first[ha_6563_ind_low:ha_6563_ind_upp]
+                
+                    plt_na_5896=ovs_data[o,:,na_5896_ind_low-plt_ex:na_5896_ind_upp+plt_ex]
+                    wav_5896=wave_first[na_5896_ind_low:na_5896_ind_upp]
+                
+                    fig,ax=plt.subplots(1,4,figsize=(12,3))
+                
+                    ax[0].plot(wave_first[o2_7594_ind_low-plt_ex:o2_7594_ind_upp+plt_ex],
+                             plt_o2_7594[0,:]/np.nanmax(plt_o2_7594[0,:]), color='black', linewidth=3.0)
+                    ax[0].plot(wave_first[o2_7594_ind_low-plt_ex:o2_7594_ind_upp+plt_ex],
+                             plt_o2_7594[t,:]/np.nanmax(plt_o2_7594[t,:]), color='red', linewidth=1.0)
+                    ax[0].axvline(x=wav_7594[o2_7594_ac_0], linestyle='--',color='grey',linewidth=1.5)
+                    ax[0].axvline(x=wav_7594[o2_7594_ac], linestyle='--',color='salmon',linewidth=0.5)
+                    ax[0].set_title('7594 O$_{2}$')
+                
+                    ax[1].plot(wave_first[o2_6867_ind_low-plt_ex:o2_6867_ind_upp+plt_ex],
+                             plt_o2_6867[0,:]/np.nanmax(plt_o2_6867[0,:]), color='black', linewidth=3.0)
+                    ax[1].plot(wave_first[o2_6867_ind_low-plt_ex:o2_6867_ind_upp+plt_ex],
+                             plt_o2_6867[t,:]/np.nanmax(plt_o2_6867[t,:]), color='red', linewidth=1.0)
+                    ax[1].axvline(x=wav_6867[o2_6867_ac_0], linestyle='--',color='grey',linewidth=1.5)
+                    ax[1].axvline(x=wav_6867[o2_6867_ac], linestyle='--',color='salmon',linewidth=0.5)
+                    ax[1].set_title('6867 O$_{2}$')
+                
+                    ax[2].plot(wave_first[ha_6563_ind_low-plt_ex:ha_6563_ind_upp+plt_ex],
+                             plt_ha_6563[0,:]/np.nanmax(plt_ha_6563[0,:]), color='black', linewidth=3.0)
+                    ax[2].plot(wave_first[ha_6563_ind_low-plt_ex:ha_6563_ind_upp+plt_ex],
+                             plt_ha_6563[t,:]/np.nanmax(plt_ha_6563[t,:]), color='red', linewidth=1.0)
+                    ax[2].axvline(x=wav_6563[ha_6563_ac_0], linestyle='--',color='grey',linewidth=1.5)
+                    ax[2].axvline(x=wav_6563[ha_6563_ac], linestyle='--',color='salmon',linewidth=0.5)
+                    ax[2].set_title('6563 H$\\alpha$')
+                
+                    ax[3].plot(wave_first[na_5896_ind_low-plt_ex:na_5896_ind_upp+plt_ex],
+                             plt_na_5896[0,:]/np.nanmax(plt_na_5896[0,:]), color='black', linewidth=3.0)
+                    ax[3].plot(wave_first[na_5896_ind_low-plt_ex:na_5896_ind_upp+plt_ex],
+                             plt_na_5896[t,:]/np.nanmax(plt_na_5896[t,:]), color='red', linewidth=1.0)
+                    ax[3].axvline(x=wav_5896[na_5896_ac_0], linestyle='--',color='grey',linewidth=1.5)
+                    ax[3].axvline(x=wav_5896[na_5896_ac], linestyle='--',color='salmon',linewidth=0.5)
+                    ax[3].set_title('5896 Na')
+                
+                    plt.show(block=False)
+                
+                
             
             shift_pixels[o,t,:]=pix_ar+shift/osr
         
@@ -295,6 +356,12 @@ def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex
         #for t in range(0,n_exp):
         #    inter=interp1d(wav_ar[o,t,:],smooth_data[o,t,:])
         #    int_data[o,t,:]=interp1d(wav_ar[o,t,:],smooth_data[o,t,:])
+        plt.figure(104,figsize=(12,3))
+        plt.plot(np.linspace(1,n_exp,n_exp)[1:],y_shift[o,1:],'.',markersize=12,color='blue')
+        plt.show(block=False)
+        
+        
+        plt.figure(105,figsize=(12,5))
         
         plt.plot(wav_ar[o,0,:],input_data[o,0,:]/np.nanmax(input_data[o,0,:]),color='black',linewidth=2.0)
         plt.plot(wav_ar[o,0,:],cnv_data[o,0,:]/np.nanmax(cnv_data[o,0,:]),color='red',linewidth=1.0)
@@ -303,7 +370,7 @@ def AlignSpec(osr,window,fwhm,fwhm_t,ks,olv,wavelength_path,obj_name,SAVEPATH,ex
         plt.axvline(x=6562.81,color='grey',linewidth=0.5,linestyle='--')
         plt.axvline(x=5895.9,color='grey',linewidth=0.5,linestyle='--')
         plt.axvline(x=5889.9,color='grey',linewidth=0.5,linestyle='--')
-        plt.show()
+        plt.show(block=False)
     if corr==True:
         np.savez_compressed(SAVEPATH+'ShiftedSpec_All_Corr.npz',data=input_data,convolved=cnv_data,pixels=shift_pixels,wave=wav_ar)
     else:
