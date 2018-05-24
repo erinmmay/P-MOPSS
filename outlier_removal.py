e7e6a7c66d63ee1dc3cc6295fb5c861934380b1f
Erin Mackenzie May <ermay@umich.edu>
2018-05-24 01:31:14 -0400

    tracks # corrected pixels

--- outlier_removal.py
+++ outlier_removal.py
@@ -13,3 +13,18 @@ def outlierr(data,ks,sd):
             data[i]=median[i]
     
     return data
+
+def outlierr_c(data,ks,sd):
+    median=medfilt(data,kernel_size=ks)
+    med_rm=data-median
+    
+    stddev=np.nanstd(med_rm)
+    med_va=np.nanmedian(med_rm)
+    
+    counter=0
+    for i in range(0,len(data)):
+        if med_rm[i]>sd*stddev+med_va or med_rm[i]<med_va-sd*stddev:
+            data[i]=median[i]
+            counter+=1
+    
+    return counter,data
