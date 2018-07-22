2295ebbcc680dea83cd072f7eae821aa55d7e850
Erin Mackenzie May <ermay@umich.edu>
2018-07-22 18:46:41 -0400

    Now saves exposure times

--- HeaderData.py
+++ HeaderData.py
@@ -14,6 +14,7 @@ def ReadHeader(data_path,SAVEPATH): #build arrays to store information from head
             file_cnt+=1
     n_exp=file_cnt/nchips
     obs_times=np.array([])                 #holds observing dates/times (ONLY SAVED ONCE)
+    exp_times=np.empty([n_exp])
     elc_noise=np.empty([nchips])                           #holds electron noise
     airmass=np.empty([n_exp])
     RA=np.empty([n_exp])
@@ -33,6 +34,7 @@ def ReadHeader(data_path,SAVEPATH): #build arrays to store information from head
                 print np.int((100*exp_cnt)/n_exp),'%'
             if c==1:
                 obs_times=np.append(obs_times,str(head['DATE-OBS']+'T'+head['TIME-OBS']))
+                exp_times[int(exp_cnt)]=head['EXPTIME']
                 #print obs_times[int(exp_cnt)]
                 airmass[int(exp_cnt)]=head['AIRMASS']
                 RA[int(exp_cnt)]=head['RA-D']
@@ -48,6 +50,7 @@ def ReadHeader(data_path,SAVEPATH): #build arrays to store information from head
     print '-->> Header Data Read'
     print '------------------------------------------'
     print '   Observing Times: obs_times[n_exp]'
+    print '   Exposure Times:  exp_times[n_exp]'
     print '   Object Airmass:  airmass[n_exp]'
     print '   RA:              RA[n_exp]'
     print '   DEC:             DEC[n_exp]'
@@ -58,7 +61,7 @@ def ReadHeader(data_path,SAVEPATH): #build arrays to store information from head
     print '   Structure Temp:  stc_temp[chip_n_exp]'
     print '   Ion Pump Press:  ion_pump[chip_n_exp]'
     print '------------------------------------------'
-    np.savez_compressed(SAVEPATH+'HeaderData.npz',n_exp=n_exp,obs_times=obs_times,airmass=airmass,RA=RA,DEC=DEC,Angle_o=Angle_o,Angle_e=Angle_e,elc_noise=elc_noise,ccd_temp=ccd_temp,stc_temp=stc_temp,ion_pump=ion_pump)
+    np.savez_compressed(SAVEPATH+'HeaderData.npz',n_exp=n_exp,obs_times=obs_times,exp_times=exp_times,airmass=airmass,RA=RA,DEC=DEC,Angle_o=Angle_o,Angle_e=Angle_e,elc_noise=elc_noise,ccd_temp=ccd_temp,stc_temp=stc_temp,ion_pump=ion_pump)
     del obs_times
     del airmass
     del elc_noise
