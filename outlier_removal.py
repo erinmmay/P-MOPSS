e146333a031132a97aadadb60d975c04acbd3445
Erin Mackenzie May <ermay@umich.edu>
2018-07-22 21:58:56 -0400

    outliers vs. model

--- outlier_removal.py
+++ outlier_removal.py
@@ -28,3 +28,18 @@ def outlierr_c(data,ks,sd):
             counter+=1
     
     return counter,data
+
+def outlierr_model(data,model,ks,sd):
+    median=np.copy(model)
+    med_rm=data-median
+    
+    stddev=np.nanstd(med_rm)
+    med_va=np.nanmedian(med_rm)
+    
+    counter=0
+    for i in range(0,len(data)):
+        if med_rm[i]>sd*stddev+med_va or med_rm[i]<med_va-sd*stddev:
+            data[i]=median[i]
+            counter+=1
+    
+    return counter,data
